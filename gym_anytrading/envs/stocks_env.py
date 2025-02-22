aadfb221b38eacbba3ce8a54d18a2668e22d6608
shungyan <shungyan@hotmail.my>
2025-02-22 14:30:49 +0800

    fix signal features bug

--- gym_anytrading/envs/stocks_env.py
+++ gym_anytrading/envs/stocks_env.py
@@ -15,26 +15,31 @@ class StocksEnv(TradingEnv):
         self.trade_fee_ask_percent = 0.005  # unit
 
     def _process_data(self):
-        prices = self.df.loc[:, 'Close'].to_numpy()
+        open_prices = self.df.loc[:, "Open"].to_numpy()
+        high_prices = self.df.loc[:, "High"].to_numpy()
+        low_prices = self.df.loc[:, "Low"].to_numpy()
+        close_prices = self.df.loc[:, "Close"].to_numpy()
+        volume = self.df.loc[:, "Volume"].to_numpy()
 
-        # Validate index (TODO: Improve validation)
-        prices[self.frame_bound[0] - self.window_size]
-        prices = prices[self.frame_bound[0] - self.window_size : self.frame_bound[1]]
+        sma_50 = self.df.loc[:, "SMA_50"].to_numpy()
+        ema_50 = self.df.loc[:, "EMA_50"].to_numpy()
+        rsi_14 = self.df.loc[:, "RSI_14"].to_numpy()
+        macd = self.df.loc[:, "MACD"].to_numpy()
+        macd_signal = self.df.loc[:, "MACD_Signal"].to_numpy()
+
+        stochastic_k = self.df.loc[:, "Stochastic_K"].to_numpy()
+        stochastic_d = self.df.loc[:, "Stochastic_D"].to_numpy()
+        upper_bb = self.df.loc[:, "Upper_BB"].to_numpy()
+        lower_bb = self.df.loc[:, "Lower_BB"].to_numpy()
+        adx = self.df.loc[:, "ADX"].to_numpy()
+        obv = self.df.loc[:, "OBV"].to_numpy()
+
+        prices[self.frame_bound[0] - self.window_size]  # validate index (TODO: Improve validation)
+        prices = prices[self.frame_bound[0]-self.window_size:self.frame_bound[1]]
 
         diff = np.insert(np.diff(prices), 0, 0)
 
-        # Select additional features from the DataFrame
-        feature_cols = [
-            "Open", "High", "Low", "Close", "Volume",
-            "SMA_50", "EMA_50", "RSI_14", "MACD", "MACD_Signal",
-            "Stochastic_K", "Stochastic_D", "Upper_BB", "Lower_BB",
-            "ADX", "OBV"
-        ]
-
-        additional_features = self.df.loc[self.frame_bound[0] - self.window_size : self.frame_bound[1], feature_cols].to_numpy()
-
-        # Stack prices, diff, and additional features
-        signal_features = np.column_stack((prices, diff, additional_features))
+        signal_features = np.column_stack((open_prices, high_prices, low_prices, close_prices, volume, diff,sma_50, ema_50, rsi_14, macd, macd_signal,stochastic_k, stochastic_d, upper_bb, lower_bb,adx, obv))
 
         return prices.astype(np.float32), signal_features.astype(np.float32)
 
